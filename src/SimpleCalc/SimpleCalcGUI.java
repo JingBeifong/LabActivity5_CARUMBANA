c2857731a39141fe40ca3d563c9b200ff54d521a
Mikhaella Jing Carumbana <emomj2009@gmail.com>
2022-12-17 17:10:03 +0800

    Rename SimpleCalc components

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -2,7 +2,7 @@ package SimpleCalc;
 
 import javax.swing.*;
 
-public class SimpleCalcGUI extends JFrame {
+public class SimpleCalcGUI {
     private JPanel mainpnl;
     private JTextField tfNumber1;
     private JComboBox cbOperations;
@@ -10,52 +10,5 @@ public class SimpleCalcGUI extends JFrame {
     private JTextField tfNumber2;
     private JTextField lblResult;
 
-    public SimpleCalcGUI() {
-        btnCompute.addActionListener((event) -> {
-            try {
-                double num1 = Double.parseDouble(tfNumber1.getText());
-                double num2 = Double.parseDouble(tfNumber2.getText());
-                String operation = (String) cbOperations.getSelectedItem();
-
-                double result = 0;
-                {
-                    switch (operation) {
-                        case "+":
-                            result = num1 + num2;
-                            break;
-                        case "-":
-                            result = num1 - num2;
-                            break;
-                        case "*":
-                            result = num1 * num2;
-                            break;
-                        case "/":
-                            if (num2 == 0) {
-                                throw new ArithmeticException("Invalid: Divisor cannot be 0");
-                            }
-                            result = num1 / num2;
-                            break;
-                    }
-                }
-                lblResult.setText(Double.toString(result));
-            } catch (NumberFormatException david) {
-                JOptionPane.showMessageDialog(mainpnl, "Invalid Input");
-            } catch (ArithmeticException lucy) {
-                JOptionPane.showMessageDialog(mainpnl, lucy.getMessage());
-            }
-        });
-    }
-        public static void main(String[] args){
-        SimpleCalcGUI app = new SimpleCalcGUI();
-        app.setContentPane(app.mainpnl);
-        app.setVisible(true);
-        app.setSize(600,300);
-        app.mainpnl.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
-        app.setTitle("Simple Calculator");
-        app.setLocationRelativeTo(null);
-        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-
-        }
 }
+
