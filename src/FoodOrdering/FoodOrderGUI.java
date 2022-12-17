260b74520a27fa30e34d712cf269c86585de5c82
Mikhaella Jing Carumbana <emomj2009@gmail.com>
2022-12-17 17:00:21 +0800

    Rename LeapYear components

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -2,9 +2,73 @@ package FoodOrdering;
 
 import javax.swing.*;
 
-public class FoodOrderGUI {
-    private JPanel panel1;
-    private JCheckBox pizzaCheckBox;
-    private JRadioButton noneRadioButton;
-    private JButton orderButton;
+public class FoodOrderGUI extends JFrame{
+    private JPanel mainpnl;
+    private JCheckBox cPizza;
+    private JRadioButton rbNone;
+    private JButton btnOrder;
+    private JCheckBox cBurger;
+    private JCheckBox cFries;
+    private JCheckBox cSoftDrinks;
+    private JCheckBox cTea;
+    private JCheckBox cSundae;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
+
+    private static final double pizza = 100;
+    private static final double burger = 80;
+    private static final double fries = 65;
+    private static final double softdrinks = 55;
+    private static final double tea = 50;
+    private static final double sundae = 40;
+
+    FoodOrderGUI(){
+        ButtonGroup falco = new ButtonGroup();
+        falco.add(rbNone);
+        falco.add(rb15);
+        falco.add(rb10);
+        falco.add(rb5);
+
+        btnOrder.addActionListener(e -> {
+            try {
+                double tamount = 0;
+                if(cBurger.isSelected())
+                    tamount += burger;
+                if (cFries.isSelected())
+                    tamount += fries;
+                if (cPizza.isSelected())
+                    tamount += pizza;
+                if (cTea.isSelected())
+                    tamount += tea;
+                if(cSoftDrinks.isSelected())
+                    tamount += softdrinks;
+                if(cSundae.isSelected())
+                    tamount += sundae;
+
+                if(rb5.isSelected())
+                    tamount -= tamount * 0.05;
+                else if(rb10.isSelected())
+                    tamount -= tamount * 0.10;
+                else if(rb15.isSelected())
+                    tamount -= tamount * 0.15;
+
+                if(tamount == 0){
+                    throw new IllegalArgumentException("Invalid Order: total must be more than 0, try ordering something.");
+                }
+                JOptionPane.showMessageDialog(mainpnl, "Total Price: Php " + String.format("%.2f", tamount));
+
+            } catch (IllegalArgumentException arasaka){
+                JOptionPane.showMessageDialog(FoodOrderGUI.this, "Invalid: Please choose something.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+    }
+    public static void main(String[] args){
+        FoodOrderGUI app = new FoodOrderGUI();
+        app.setContentPane(new FoodOrderGUI().mainpnl);
+        app.setVisible(true);
+        app.setSize(500, 500);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
 }
--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -2,9 +2,60 @@ package SimpleCalc;
 
 import javax.swing.*;
 
-public class SimpleCalcGUI {
-    private JPanel panel1;
-    private JTextField textField1;
-    private JComboBox comboBox1;
-    private JButton computeResultButton;
+public class SimpleCalcGUI extends JFrame {
+    private JPanel mainpnl;
+    private JTextField tfNumber1;
+    private JComboBox cbOperations;
+    private JButton btnCompute;
+    private JTextField tfNumber2;
+    private JTextField lblResult;
+
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener((event) -> {
+            try {
+                double num1 = Double.parseDouble(tfNumber1.getText());
+                double num2 = Double.parseDouble(tfNumber2.getText());
+                String operation = (String) cbOperations.getSelectedItem();
+
+                double result = 0;
+                {
+                    switch (operation) {
+                        case "+":
+                            result = num1 + num2;
+                            break;
+                        case "-":
+                            result = num1 - num2;
+                            break;
+                        case "*":
+                            result = num1 * num2;
+                            break;
+                        case "/":
+                            if (num2 == 0) {
+                                throw new ArithmeticException("Invalid: Divisor cannot be 0");
+                            }
+                            result = num1 / num2;
+                            break;
+                    }
+                }
+                lblResult.setText(Double.toString(result));
+            } catch (NumberFormatException david) {
+                JOptionPane.showMessageDialog(mainpnl, "Invalid Input");
+            } catch (ArithmeticException lucy) {
+                JOptionPane.showMessageDialog(mainpnl, lucy.getMessage());
+            }
+        });
+    }
+        public static void main(String[] args){
+        SimpleCalcGUI app = new SimpleCalcGUI();
+        app.setContentPane(app.mainpnl);
+        app.setVisible(true);
+        app.setSize(600,300);
+        app.mainpnl.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+        app.setTitle("Simple Calculator");
+        app.setLocationRelativeTo(null);
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+        }
 }
