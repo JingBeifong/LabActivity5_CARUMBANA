cf17333a86d0f8bab6160aa8729953c4c2ed1783
Mikhaella Jing Carumbana <emomj2009@gmail.com>
2022-12-17 17:19:37 +0800

    Rename FoodOrder components

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -2,7 +2,7 @@ package FoodOrdering;
 
 import javax.swing.*;
 
-public class FoodOrderGUI extends JFrame{
+public class FoodOrderGUI {
     private JPanel mainpnl;
     private JCheckBox cPizza;
     private JRadioButton rbNone;
@@ -16,59 +16,4 @@ public class FoodOrderGUI extends JFrame{
     private JRadioButton rb10;
     private JRadioButton rb15;
 
-    private static final double pizza = 100;
-    private static final double burger = 80;
-    private static final double fries = 65;
-    private static final double softdrinks = 55;
-    private static final double tea = 50;
-    private static final double sundae = 40;
-
-    FoodOrderGUI(){
-        ButtonGroup falco = new ButtonGroup();
-        falco.add(rbNone);
-        falco.add(rb15);
-        falco.add(rb10);
-        falco.add(rb5);
-
-        btnOrder.addActionListener(e -> {
-            try {
-                double tamount = 0;
-                if(cBurger.isSelected())
-                    tamount += burger;
-                if (cFries.isSelected())
-                    tamount += fries;
-                if (cPizza.isSelected())
-                    tamount += pizza;
-                if (cTea.isSelected())
-                    tamount += tea;
-                if(cSoftDrinks.isSelected())
-                    tamount += softdrinks;
-                if(cSundae.isSelected())
-                    tamount += sundae;
-
-                if(rb5.isSelected())
-                    tamount -= tamount * 0.05;
-                else if(rb10.isSelected())
-                    tamount -= tamount * 0.10;
-                else if(rb15.isSelected())
-                    tamount -= tamount * 0.15;
-
-                if(tamount == 0){
-                    throw new IllegalArgumentException("Invalid Order: total must be more than 0, try ordering something.");
-                }
-                JOptionPane.showMessageDialog(mainpnl, "Total Price: Php " + String.format("%.2f", tamount));
-
-            } catch (IllegalArgumentException arasaka){
-                JOptionPane.showMessageDialog(FoodOrderGUI.this, "Invalid: Please choose something.", "Error", JOptionPane.ERROR_MESSAGE);
-            }
-        });
-    }
-    public static void main(String[] args){
-        FoodOrderGUI app = new FoodOrderGUI();
-        app.setContentPane(new FoodOrderGUI().mainpnl);
-        app.setVisible(true);
-        app.setSize(500, 500);
-        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-    }
 }
